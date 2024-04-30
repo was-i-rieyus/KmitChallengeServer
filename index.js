85d2f6c1fffc4f445c2d8b9b334e44089faab4ca
Rieyu <21bd1a0505csea@gmail.com>
2024-04-30 11:56:49 +0530

    Added Server

---
+++ index.js
@@ -0,0 +1,69 @@
+// express-server/app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://npandramishi:123456a@cluster0.lqn9ilv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Define counter schema and model
+const counterSchema = new mongoose.Schema({
+    count: { type: Number, default: 0 }
+},{ collection: 'counters' });
+const Counter = mongoose.model('Counter', counterSchema);
+
+// Routes
+app.get('/api/counter', async (req, res) => {
+    console.log("Reached GET method")
+    try {
+        
+        const counter = await Counter.findOne();
+        console.log(counter);
+        res.json(counter);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+app.post('/api/counter/increment', async (req, res) => {
+    try {
+        let counter = await Counter.findOne();
+        if (!counter) {
+            counter = new Counter();
+        }
+        counter.count++;
+        await counter.save();
+        res.json(counter);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+app.post('/api/counter/decrement', async (req, res) => {
+    try {
+        let counter = await Counter.findOne();
+        if (!counter) {
+            counter = new Counter();
+        }
+        counter.count--;
+        await counter.save();
+        res.json(counter);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
